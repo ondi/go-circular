409c96cac419ab15060fd84cc57fff36bed6176f
ondi <thy321@gmail.com>
2019-09-01 22:54:36 +0300

    minor fix

--- circular.go
+++ circular.go
@@ -23,11 +23,11 @@ func (self * List_t) PushBack(value interface{}) bool {
 	if self.written == self.limit {
 		return false
 	}
-	if self.head == self.limit {
-		self.head = 0
+	if self.tail == self.limit {
+		self.tail = 0
 	}
-	self.root[self.head] = value
-	self.head++
+	self.root[self.tail] = value
+	self.tail++
 	self.written++
 	return true
 }
@@ -36,11 +36,11 @@ func (self * List_t) PopFront() (value interface{}, ok bool) {
 	if self.written == 0 {
 		return
 	}
-	if self.tail == self.limit {
-		self.tail = 0
+	if self.head == self.limit {
+		self.head = 0
 	}
-	value = self.root[self.tail]
-	self.tail++
+	value = self.root[self.head]
+	self.head++
 	self.written--
 	ok = true
 	return
@@ -50,12 +50,12 @@ func (self * List_t) PushFront(value interface{}) bool {
 	if self.written == self.limit {
 		return false
 	}
-	if self.tail == 0 {
-		self.tail = self.limit - 1
+	if self.head == 0 {
+		self.head = self.limit - 1
 	} else {
-		self.tail--
+		self.head--
 	}
-	self.root[self.tail] = value
+	self.root[self.head] = value
 	self.written++
 	return true
 }
@@ -64,17 +64,17 @@ func (self * List_t) PopBack() (interface{}, bool) {
 	if self.written == 0 {
 		return nil, false
 	}
-	if self.head == 0 {
-		self.head = self.limit - 1
+	if self.tail == 0 {
+		self.tail = self.limit - 1
 	} else {
-		self.head--
+		self.tail--
 	}
 	self.written--
-	return self.root[self.head], true
+	return self.root[self.tail], true
 }
 
 func (self * List_t) RangeFront(f func(interface{}) bool) {
-	cur := self.tail
+	cur := self.head
 	for i := 0; i < self.written; i++ {
 		if cur == self.limit {
 			cur = 0
@@ -87,7 +87,7 @@ func (self * List_t) RangeFront(f func(interface{}) bool) {
 }
 
 func (self * List_t) RangeBack(f func(interface{}) bool) {
-	cur := self.head
+	cur := self.tail
 	for i := 0; i < self.written; i++ {
 		if cur == 0 {
 			cur = self.limit - 1
